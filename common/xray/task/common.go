e2a1cfd5a8c836391251c8bad6a9bc770cb3e572
CloudPassenger <coliwatoshinzo@gmail.com>
2026-06-01 11:28:23 +0100

    lint: modernize xhttp helpers

--- common/xray/task/common.go
+++ common/xray/task/common.go
@@ -3,7 +3,7 @@ package task
 import "github.com/sagernet/sing-box/common/xray"
 
 // Close returns a func() that closes v.
-func Close(v interface{}) func() error {
+func Close(v any) func() error {
 	return func() error {
 		return common.Close(v)
 	}
